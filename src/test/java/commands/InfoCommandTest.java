bbc1962254828dae913d5cf528d68b757c7103c6
abalakin <abalakin@diasoft.ru>
2026-02-22 13:32:45 +0300

    Че этим сборкам в тесте не нравится

--- src/test/java/commands/InfoCommandTest.java
+++ src/test/java/commands/InfoCommandTest.java
@@ -226,6 +226,7 @@ public class InfoCommandTest {
         cmd.execute();
 
         assertFalse(messages.isEmpty());
+        System.out.println(messages.get(0));
 //        assertTrue(messages.get(0).contains("15 дн."));
         assertTrue(messages.get(0).contains("50%"));
     }
